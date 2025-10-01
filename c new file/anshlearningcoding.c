d32ecf0a60e7c94e5f02e8e209c64f21402d7d21
anshpratapsingh6390-dot <anshpratapsingh6390@gmail.com>
2025-10-02 00:33:11 +0530

    anshlearningcoding.c
    practise mode

---
+++ c new file/anshlearningcoding.c
@@ -0,0 +1,257 @@
+// #include<stdio.h>
+
+// int main(){
+//     int length, width , area ;
+//     printf(" enter  your length : ");
+//     printf(" enter  your width : ");
+//     scanf("%d,%d",&length,&width);
+
+//     area = length*width;
+
+//     printf("The Area of the Given values = %d",area);
+
+
+    
+// }
+
+
+
+// #include <stdio.h>
+// int main() {
+// float r= 1.0,height=1.0;
+// printf("areaof circle of radius %.2f is %.2f/n" , r,3.14*r*r);
+// printf("vol of cylinder with radius %.2f and height %f is %f/n",r,height,3.14*r*r*height);
+
+// return 0;
+// }
+
+
+
+
+// #include <stdio.h>
+// int  main() {
+// int a ;
+// printf("enter a = ");
+// scanf("%d",&a);
+
+// switch(a) {
+// case 1:
+// printf("you enter 1/n");
+// break;
+// case 2:
+// printf("you enter 2/n");
+// break;
+// case 3:
+// printf(" your enter 3/n");
+// break;
+
+
+// case 4: 
+// printf("you enter 4/n");
+// break;
+
+// default :
+// printf("stop it here/n");
+
+// }
+
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+// write a program toassingh marks to the student of the class in the exam.
+
+// #include<stdio.h>
+// int main() {   
+// char grade;
+// int marks = 73;
+
+// if ( marks >=90 && marks <=100)
+//     printf("grade = A");  
+
+// else if(marks >=80 && marks <=90)
+//     printf(" grade = B"); 
+
+// else if (marks >=70 && marks <=80)
+//     printf("grade = C");
+
+
+// else if(marks >=60 && marks <=70)
+//     printf("grade = D");
+
+
+// else if(marks >=50 && marks <=60)
+//     printf("grade = E");
+
+// else
+//     printf("fail/n");
+
+// }
+
+
+// Question that is puzzling
+
+
+
+// #include<stdio.h>
+// int main()
+// { int a = 10;
+//     if ( a = 11)
+//          printf("iam 11");
+
+//     else
+//         printf("i am  not 11");
+
+//out put ayega i am 11 kyuki a= 11 bss true hai yeh darshata hai 
+
+// }
+
+
+
+
+
+// #include<stdio.h>
+// int main()
+// { int a = 10;
+//     if ( a == 11)
+//          printf("iam 11");
+
+//     else
+//         printf("i am  not 11");
+
+// }
+
+// ab jo output bo hoga i am not 11 bcz ab maine a == 11 value assign kr dia hai jo ki pjhle nhi ki thi 
+
+
+// #include <stdio.h>
+// int main(){
+//     float marks1, marks2 , marks3 , total , percentage;
+
+//     printf("enter your marks for subject1:\n");
+//     scanf("%f",&marks1);
+
+//     printf("enter your marks for subject2:\n");
+//     scanf("%f",&marks2);
+
+//     printf("enter your marks for subject3:\n");
+//     scanf("%f",&marks3);
+
+//     printf("marks entered are  %.2f,%.2f,%.2f\n", marks1, marks2, marks3 );
+
+// total = marks1 + marks2 +marks3 ;
+// percentage = total/3;
+
+
+//     if ( marks1 <33 || marks2 <33 || marks3 <33 )
+//    { printf("you are fail due to less marks in indivisual subjects\n");}
+
+//     else if((marks1 + marks2 + marks3 )/3 <40 )
+//    { printf("you are failed due to less percentage\n");}
+
+//     else { 
+//         printf("passed with %.2f\n");
+        
+//      }
+
+//     }
+
+
+
+// QUES CALCULATE Tax ?
+
+  
+// #include<stdio.h>
+// int main(){
+//     double income, tax =0;
+
+// printf("enter income = \n");
+// scanf("%lf", &income);
+
+
+// if (income<=250000)
+// {  tax = 0;   }
+
+// else if ( income>250000 && income<=500000 )
+// {  tax = 0.05*(income-250000); }
+
+// else if ( income>500000 && income <=1000000)
+// {    tax = 0.05*(500000 -250000) + 0.2*(income -500000);  }
+
+// else 
+// {    tax = 0.05*(500000-250000) +0.2*(1000000 -500000) +0.3*(income -1000000);     }
+//  printf(" the total tax you need to pay is %.2f ", tax );
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
